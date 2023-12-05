4987b001223bc459112d2624fb1b13db07ae2646
zeniavarela <zvarela8@gmail.com>
2023-12-05 15:55:55 -0800

    validations

--- spec/requests/sightings_spec.rb
+++ spec/requests/sightings_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Sightings", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  RSpec.describe 'Sightings API', type: :request do
+    describe 'POST /sightings' do
+      it 'returns a 422 status code with validation errors' do
+        post '/sightings', params: { sighting: { latitude: 45.5231, longitude: -122.6765 } }
+        expect(response).to have_http_status(422)
+        expect(JSON.parse(response.body)).to have_key('errors')
+      end
+    end
   end
-end
